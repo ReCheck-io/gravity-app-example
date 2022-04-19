92616a91e1fc102d815b418a7d843c79ce8a02de
Byurhan Beyzat <imbyurhan@gmail.com>
2022-04-19 14:42:00 +0300

    Initial Commit

---
+++ web3-demo/web3_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  web3_demoApp.swift
+//  web3-demo
+//
+//  Created by Byurhan Beyzat on 19.04.22.
+//
+
+import SwiftUI
+
+@main
+struct web3_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
