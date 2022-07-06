940b08b08d4a9ed70a1c7f31334b3722640f503d
Byurhan Beyzat <imbyurhan@gmail.com>
2022-07-06 12:06:34 +0300

    Update sign and verify functions

--- web3-demo/ContentView.swift
+++ web3-demo/ContentView.swift
@@ -16,9 +16,9 @@ extension Data {
 
 struct ContentView: View {
     let web3p = Web3Provider()
+    let geip712 = GEIP712()
     
     var accountPassword = "123123123"
-    var privateKeyForImportWallet = "b142d54571d9f9775659564d284208a828e5d211409bb18e053f045c0176f8a6"
     var mnemonicsForImportWallet = "fluid kind bird ice wing ribbon era common scissors stock chat estate"
     
     @State var address: String = "Wallet: N/A"
@@ -44,14 +44,11 @@ struct ContentView: View {
             
             Button("Sign Terms") {
                 print("Sign Terms (Gassless)....")
-                
-                let geip712 = GEIP712()
-                
+
                 try? geip712.signTerms(
                     keystore: web3p.wallet.keystore!,
                     keyStorePassword: accountPassword,
-                    termsHash: "0x4f18e5cf5b77b13fb6c80122b3cde9697e7b0a35aef062ed33b683fbf072489b",
-                    termsVersion: "0x4f18e5cf5b77b13fb6c80122b3cde9697e7b0a35aef062ed33b683fbf072489b")
+                    tosHash: "0x4f18e5cf5b77b13fb6c80122b3cde9697e7b0a35aef062ed33b683fbf072489b")
             }
             .padding(.horizontal, 12.0)
             .padding(.vertical, 8.0)
@@ -60,32 +57,8 @@ struct ContentView: View {
             
             Button("Verify Signature") {
                 print("Verify Signature....")
-//                let walletAddress = EthereumAddress(web3p.wallet.address)!
-//
-//                guard let url = Bundle.main.url(forResource: "ContractABI", withExtension: "json") else {
-//                    print("File not found")
-//                    return
-//                }
-//
-//                let contractABI = try! String(contentsOf: url)
-//                let contract = web3p.client.contract(contractABI, at: web3p.contractAddress, abiVersion: 2)!
-//
-//                let parameters = [
-//                    walletAddress.address,
-//                    "0x4f18e5cf5b77b13fb6c80122b3cde9697e7b0a35aef062ed33b683fbf072489b",
-//                    "0x4f18e5cf5b77b13fb6c80122b3cde9697e7b0a35aef062ed33b683fbf072489b"]
-//
-//                var options = TransactionOptions.defaultOptions
-//                options.from = walletAddress
-//                options.gasPrice = .automatic
-//                options.gasLimit = .automatic
-//
-//                let tx = contract.read("validateSignature", parameters: parameters as [AnyObject], extraData: Data() as Data,transactionOptions: options)!
-//
-//                let res = try? tx.call()
-//
-//                print(res as Any)
                 
+                try? geip712.verifyTermsSignature(keystore: web3p.wallet.keystore!, tosHash: "0x4f18e5cf5b77b13fb6c80122b3cde9697e7b0a35aef062ed33b683fbf072489b")
             }
             .padding(.horizontal, 12.0)
             .padding(.vertical, 8.0)
--- web3-demo/GEIP712.swift
+++ web3-demo/GEIP712.swift
@@ -9,7 +9,7 @@ import Foundation
 import web3swift
 
 struct RelayerPayload: Codable {
-    let tx: SafeTxStr
+    let tosHash: String
     let from: EthereumAddress
     let signature: String
 }
@@ -21,18 +21,76 @@ class GEIP712 {
 //    let relayContract: String = "0xaC4c847899f7A38b166DCcb83171eF4c19FD4c9C"
 //    let tosContract: String = "0x3F06bAAdA68bB997daB03d91DBD0B73e196c5A4d"
     
-    let tosContract: String = "0x5DCC06c74BCaBb840B08F05399A44AEc3ED1bdD4"
-    let relayContract: String = "0xaC4c847899f7A38b166DCcb83171eF4c19FD4c9C"
+//    let tosContract: String = "0x5DCC06c74BCaBb840B08F05399A44AEc3ED1bdD4"
+//    let relayContract: String = "0xaC4c847899f7A38b166DCcb83171eF4c19FD4c9C"
     
 //    let tosContract: String = "0x3F06bAAdA68bB997daB03d91DBD0B73e196c5A4d"
 //    let relayContract: String = "0x3F06bAAdA68bB997daB03d91DBD0B73e196c5A4d"
     let relayUrl: String = "https://gravity-relay.recheck.io"
-    let chainId: EIP712.UInt256 = .init(337)
+    
+    
 
-    func getUserNonce(userAddress: EthereumAddress) throws -> EIP712.UInt256 {
-        let url = relayUrl + "/nonce"
+    func signTerms(keystore: BIP32Keystore, keyStorePassword: String, tosHash: String) throws {
+        let userAddress = keystore.addresses?[0]
+
+        let signature = try Web3Signer.signPersonalMessage(
+            tosHash.data(using: .utf8)!,
+            keystore: keystore,
+            account: userAddress!,
+            password: keyStorePassword
+        )
+        
+        let hashSignature = "0x" + (signature?.toHexString())!
+        print("hashSignature:", hashSignature)
+        
+        let signee = Web3Utils.personalECRecover(tosHash.data(using: .utf8)!, signature: signature!)
+        print("Recovered address:", signee as Any);
+        
+        let payload = RelayerPayload(tosHash: tosHash, from: userAddress!, signature: hashSignature)
+
+        submitRelayRequest(object: payload)
+        
+        func submitRelayRequest(object: RelayerPayload) {
+            do {
+                let jsonData = try JSONEncoder().encode(object)
+                let url = URL(string: relayUrl + "/relay")!
+                var request = URLRequest(url: url)
+                
+                request.httpBody = jsonData
+                request.httpMethod = "POST"
+                request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+                request.setValue("application/json", forHTTPHeaderField: "Accept")
+
+                let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                    guard
+                        let data = data,                              // is there data
+                        let response = response as? HTTPURLResponse,  // is there HTTP response
+                        200 ..< 300 ~= response.statusCode,           // is statusCode 2XX
+                        error == nil                                  // was there no error
+                    else {
+                        return
+                    }
+                    
+                    let responseObject = (try? JSONSerialization.jsonObject(with: data)) as? [String: Any]
+                    print("Tx response: ", responseObject as Any)
+                }
+
+                task.resume()
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    func verifyTermsSignature(keystore: BIP32Keystore, tosHash: String) throws {
+        let userAddress = keystore.addresses?[0]
+        
+        let url = relayUrl + "/verifySignature"
         var components = URLComponents(string: url)!
-        components.queryItems = [URLQueryItem(name: "account", value: userAddress.address)]
+        components.queryItems = [
+            URLQueryItem(name: "tosHash", value: tosHash),
+            URLQueryItem(name: "account", value: userAddress?.address)
+        ]
         
         components.percentEncodedQuery = components.percentEncodedQuery?.replacingOccurrences(of: "+", with: "%2B")
 
@@ -41,8 +99,6 @@ class GEIP712 {
         request.httpMethod = "GET"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.setValue("application/json", forHTTPHeaderField: "Accept")
-        
-        var nonceValue = 0;
         
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
             guard
@@ -55,121 +111,9 @@ class GEIP712 {
             }
             
             let responseObject = (try? JSONSerialization.jsonObject(with: data)) as? [String: Any]
-            print("Nonce response: ", responseObject as Any)
-            
-            nonceValue = (responseObject?["result"] as? NSString)?.integerValue ?? 0
+            print("Verify Signature Response: ", responseObject as Any)
         }
 
         task.resume()
-        
-        return .init(nonceValue)
-    }
-
-    func submitJson(object: RelayerPayload) {
-        do {
-            let jsonData = try JSONEncoder().encode(object)
-//            print("Request Body: ", object)
-            
-            let url = URL(string: relayUrl + "/relay")!
-            var request = URLRequest(url: url)
-            request.httpMethod = "POST"
-            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.setValue("application/json", forHTTPHeaderField: "Accept")
-
-            // insert json data to the request
-            request.httpBody = jsonData
-
-            let task = URLSession.shared.dataTask(with: request) { data, response, error in
-                guard
-                    let data = data,                              // is there data
-                    let response = response as? HTTPURLResponse,  // is there HTTP response
-                    200 ..< 300 ~= response.statusCode,           // is statusCode 2XX
-                    error == nil                                  // was there no error
-                else {
-                    return
-                }
-                
-                let responseObject = (try? JSONSerialization.jsonObject(with: data)) as? [String: Any]
-                print("Tx response: ", responseObject as Any)
-            }
-
-            task.resume()
-        } catch {
-            print(error)
-        }
-    }
-
-    func signTerms(keystore: BIP32Keystore, keyStorePassword: String, termsHash: String, termsVersion: String) throws {
-        let userAddress = keystore.addresses?[0]
-        let to = EthereumAddress(tosContract)!
-        let value = EIP712.UInt256(0)
-
-        let function = ABI.Element.Function(
-            name: "signTerms",
-            inputs: [
-                .init(name: "termsHash", type: .string),
-                .init(name: "termsVersion", type: .string)],
-            outputs: [],
-            constant: false,
-            payable: false)
-
-        let object = ABI.Element.function(function)
-
-        let safeTxData = object.encodeParameters([
-            termsHash as AnyObject,
-            termsVersion as AnyObject
-        ])!
-
-        let nonce: EIP712.UInt256 = try! getUserNonce(userAddress: userAddress!)
-        
-        let operation: EIP712.UInt8 = 1
-        let baseGas = EIP712.UInt256("60000")
-        let safeTxGas = EIP712.UInt256("250000")
-        let gasPrice = EIP712.UInt256("20000000000")
-        let gasToken = EthereumAddress("0x0000000000000000000000000000000000000000")!
-       
-        let safeTX = SafeTx(
-            to: to,
-            value: value,
-            data: safeTxData,
-            operation: operation,
-            safeTxGas: safeTxGas,
-            baseGas: baseGas,
-            gasPrice: gasPrice,
-            gasToken: gasToken,
-            refundReceiver: userAddress!,
-            nonce: nonce)
-
-        let signature = try Web3Signer.signEIP712(
-            safeTx: safeTX,
-            keystore: keystore,
-            verifyingContract: EthereumAddress(relayContract)!,
-            account: userAddress!,
-            password: keyStorePassword,
-            chainId: chainId)
-            
-//            name: "AwlForwarder",
-//            version: "1",
-//            typeHash: safeTX.encodeType()
-//            ForwardRequest(address to,uint256 value,bytes data,uint8 operation,uint256 safeTxGas,uint256 baseGas,uint256 gasPrice,address gasToken,address refundReceiver,uint256 nonce)
-        
-//        print("TypeHash", safeTX.encodeType())
-        
-
-        let safeTxObject = SafeTxStr(
-            to: to,
-            value: value,
-            data: safeTxData,
-            operation: operation,
-            safeTxGas: safeTxGas,
-            baseGas: baseGas,
-            gasPrice: gasPrice,
-            gasToken: gasToken,
-            refundReceiver: userAddress!,
-            nonce: nonce)
-        
-        let payload = RelayerPayload(tx: safeTxObject, from: userAddress!, signature: "0x" + signature.toHexString())
-        
-        submitJson(object: payload)
     }
 }
