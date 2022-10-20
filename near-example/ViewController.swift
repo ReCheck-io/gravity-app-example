7b966e51c3ed9635faa698f4ba8ee17e01d0aba6
Byurhan Beyzat <imbyurhan@gmail.com>
2022-10-20 11:39:05 +0300

    Initial Commit

---
+++ near-example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  near-example
+//
+//  Created by Byurhan Beyzat on 20.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
